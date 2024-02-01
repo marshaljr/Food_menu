2e945174853abfddf0151ebba7d889894075c7b8
mac <mac@macs-MacBook-Pro.local>
2024-02-01 16:29:33 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// let btns = document.getElementById("content")
+
+// button.addEventListener("click", function(){
+//     if(btns==btn1){
+//         document.innerHTML=index.html
+//     }
+//     else if (btns==btn2){
+//         document.innerHTML=service.html
+//     }
+// })
+
+// let links = document.querySelectorAll("content")
+
+// function navigateToPage(page){
+//     window.location.href = page;
+// }
+//  links.forEach(link=>{
+//     link.addEventListener('click',navigateToPage)
+//  })
